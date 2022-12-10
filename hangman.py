a969567e53da9ea73fd9f51eb81430a560b6d1a5
Adir Ben-David <91300383+beezzyy@users.noreply.github.com>
2022-12-10 00:03:44 -0500

    added guessed letter table and added new words

--- hangman.py
+++ hangman.py
@@ -8,8 +8,9 @@ logo = hangman_art.logo
 
 print(logo)
 
-word_list = hangman_words.word_list
+word_list = hangman_words.get_words()
 display = []
+guessed_letter = []
 lives = 6
 
 chosen_word = random.choice(word_list)
@@ -19,9 +20,40 @@ for x in range(len(chosen_word)):
 
 end_of_game = False
 
+
+def replay(logo):
+    replay = input("\nDo you want to play again? (y/n): ").lower()
+    if replay == "y":
+        os.system('cls')
+        print(logo)
+        word_list = hangman_words.get_words()
+        global display
+        display = []
+        global lives
+        lives = 6
+        global guessed_letter
+        guessed_letter = []
+
+        global chosen_word
+        chosen_word = random.choice(word_list)
+
+        for x in range(len(chosen_word)):
+            display.append("_")
+
+        global end_of_game
+        end_of_game = False
+    else:
+        print("\nThanks for playing!")
+        exit(0)
+
+
 while not end_of_game:
     guess = input("Guess a letter: ").lower()
+    guessed_letter.append(guess)
     os.system('cls')
+
+    print("\t\t\t\t\t\t\t\t\t Guessed Letter: ", guessed_letter)
+
     if guess in display:
         print(f"You've already guessed {guess}")
 
@@ -45,8 +77,9 @@ while not end_of_game:
     if "_" not in display:
         end_of_game = True
         print("YOU WIN!")
+        replay(logo)
 
-if lives == 0:
-    print("YOU LOST")
-    print(f"The word was: {chosen_word}")
-k = input("press enter to exit")
+    if lives == 0:
+        print("YOU LOST\n")
+        print(f"The word was: {chosen_word}")
+        replay(logo)
